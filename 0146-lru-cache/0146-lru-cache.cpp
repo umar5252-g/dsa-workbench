c7373159178cc6dedee0d5e2a2464535e970c721
Umar Khan <mromertariq@gmail.com>
2026-08-25 20:05:24 +0500

    Time: 34 ms (88.92%), Space: 51.1 MB (88.55%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -2,11 +2,12 @@ class LRUCache {
 public:
     class Node {
         public:
-        int key, val;
+        int key;
+        int val;
         Node* next;
-        Node* prev;
+        Node*prev;
 
-        Node(int k, int v) {
+        Node(int k,int v){
             key = k;
             val = v;
             next = prev = NULL;
@@ -14,54 +15,57 @@ public:
     };
 
     void addNode(Node* newNode) {
-        Node* oldNode = head->next;
-        head->next = newNode;
-        newNode->prev = head;
-        newNode->next = oldNode;
-        oldNode->prev = newNode;
+      Node* oldNode = head->next;
+      head->next = newNode;
+      newNode->next = oldNode;
+      oldNode->prev = newNode;
+      newNode->prev = head;
     }
 
-    void delNode(Node* delNext) {
-        Node* oldPrev = delNext->prev;
-        Node* oldNext = delNext->next;
+    void delNode(Node* oldNode) {
+        Node* oldPrev = oldNode->prev;
+        Node* oldNext = oldNode->next;
         oldPrev->next = oldNext;
         oldNext->prev = oldPrev;
+        
     }
 
-    Node* head = new Node(-1, -1);
+    unordered_map<int, Node*>m;
+    Node* head = new Node(-1,-1);
     Node* tail = new Node(-1, -1);
-    unordered_map<int, Node*> m;
-    int limit ;
+    int limit;
     LRUCache(int capacity) {
-         limit = capacity;
+        limit =capacity;
         head->next = tail;
         tail->prev = head;
+        
     }
 
     int get(int key) {
-        if (m.find(key) == m.end()) {
+        if(m.find(key) == m.end()){
             return -1;
         }
-        Node* ansNode = m[key];
-        int ans = ansNode->val;
 
+       Node* ansNode = m[key];
+       int ans = ansNode->val;
         delNode(ansNode);
         m.erase(key);
-
         addNode(ansNode);
         m[key] = ansNode;
-        return ans;
+       return ans;
     }
 
     void put(int key, int value) {
-        if (m.find(key) != m.end()) {
-            Node* oldNode = m[key];
-            m.erase(key);
+        if(m.find(key) != m.end()){
+           Node* oldNode = m[key];
             delNode(oldNode);
+            m.erase(key);
         }
-        if (m.size() == limit) {
-            m.erase(tail->prev->key);
-            delNode(tail->prev);
+        if(m.size()== limit){
+           Node* delNext = tail->prev;
+            delNode(delNext);
+            m.erase(delNext->key);
+            
         }
         Node* newNode = new Node(key, value);
         addNode(newNode);
